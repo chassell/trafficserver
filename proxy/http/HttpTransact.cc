9b8ca30a634cda028305a16aa290275c438e065e
John Rushford <John_Rushford@cable.comcast.com>
2015-06-18 14:49:45 +0000

    make parameters for parent selection overridable.

--- proxy/http/HttpTransact.cc
+++ proxy/http/HttpTransact.cc
@@ -3478,6 +3478,7 @@ HttpTransact::handle_response_from_parent(State *s)
     DebugTxn("http_trans", "[hrfp] connection alive");
     s->current.server->connect_result = 0;
     SET_VIA_STRING(VIA_DETAIL_PP_CONNECT, VIA_DETAIL_PP_SUCCESS);
+    DebugTxn("http_trans", "[hrfp] s->parent_result.retry = %d", s->parent_result.retry);
     if (s->parent_result.retry) {
       s->parent_params->recordRetrySuccess(&s->parent_result);
     }
@@ -6422,11 +6423,19 @@ HttpTransact::is_response_valid(State *s, HTTPHdr *incoming_response)
   }
 
   // is this response is from a load balanced parent.
+  DebugTxn("parent_select", "s->txn_conf->simple_retry_response_codes_string = %s\n", 
+    s->txn_conf->simple_retry_response_codes_string);
+  DebugTxn("parent_select", "s->txn_conf->dead_server_retry_response_codes_string = %s\n", 
+    s->txn_conf->dead_server_retry_response_codes_string);
+  DebugTxn("parent_select", "s->txn_conf->simple_retry_enabled = %d\n", s->txn_conf->simple_retry_enabled);
+  DebugTxn("parent_select", "s->txn_conf->dead_server_retry_enabled = %d\n", s->txn_conf->dead_server_retry_enabled);
   if (s->current.request_to == PARENT_PROXY && s->parent_result.r == PARENT_ORIGIN) {
     server_response = http_hdr_status_get(s->hdr_info.server_response.m_http);
     DebugTxn("http_trans", "[is_response_valid] server_response = %d\n", server_response);
     // is a simple retry required.
-    if (s->txn_conf->simple_retry_enabled && s->http_config_param->simple_retry_response_codes->contains(server_response)) {
+    if (s->txn_conf->simple_retry_enabled && 
+            s->http_config_param->response_codes->contains(server_response, s->txn_conf->simple_retry_response_codes_string)) {
+      DebugTxn("parent_select", "GOT A SIMPLE RETRY RESPONSE");
       // initiate a retry if we have not already tried all parents, otherwise the response is sent to the client as is.
       // see SIMPLE_RETRY in handle_response_from_parent().
       if (s->current.simple_retry_attempts < s->parent_result.rec->num_parents) {
@@ -6438,7 +6447,7 @@ HttpTransact::is_response_valid(State *s, HTTPHdr *incoming_response)
     }
     // is a dead server retry required.
     else if (s->txn_conf->dead_server_retry_enabled &&
-             s->http_config_param->dead_server_retry_response_codes->contains(server_response)) {
+            s->http_config_param->response_codes->contains(server_response, s->txn_conf->dead_server_retry_response_codes_string)) {
       // initiate a dead server retry if we have not already tried all parents, otherwise the response is sent to the client as is.
       // see DEAD_SERVER_RETRY in handle_response_from_parent().
       if (s->current.dead_server_retry_attempts < s->parent_result.rec->num_parents) {
