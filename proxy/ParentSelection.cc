1f0ca92c9bf313f2ee41af0c27d8307a7d06310c
John Rushford <John_Rushford@cable.comcast.com>
2015-07-31 19:57:12 +0000

    fixed wrap_around usage in ParentConsistentHash::lookup()

--- proxy/ParentSelection.cc
+++ proxy/ParentSelection.cc
@@ -300,9 +300,6 @@ ParentConsistentHash::lookupParent(bool first_call, ParentResult *result, Reques
 
   ink_assert(numParents() > 0 || go_direct == true);
 
-  //Debug("cdn", "In ParentConsistentHash::lookupParent(): cur_index=%d,last_lookup=%d,last_parent=%d,start_parent=%d,wrap_around=%d",
-  //cur_index,last_lookup,last_parent[last_lookup],start_parent[last_lookup],
-  //wrap_around[last_lookup]);
   if (first_call) {  // First lookup (called by findParent()).
     // We should only get into this state if
     //  we are supposed to go direct.
@@ -429,7 +426,7 @@ ParentConsistentHash::lookupParent(bool first_call, ParentResult *result, Reques
     }
 
     do {
-      prtmp = (pRecord *)chash[last_lookup]->lookup(NULL, 0, &(chashIter[last_lookup]), &result->wrap_around);
+      prtmp = (pRecord *)chash[last_lookup]->lookup(NULL, 0, &(chashIter[last_lookup]), &wrap_around[last_lookup]);
     } while (prtmp && foundParents[last_lookup][prtmp->idx]);
 
     if (prtmp) {
@@ -437,7 +434,13 @@ ParentConsistentHash::lookupParent(bool first_call, ParentResult *result, Reques
       foundParents[last_lookup][cur_index] = true;
       start_parent[last_lookup]++;
     }
-  } while (result->wrap_around);
+  } while (wrap_around[last_lookup]);
+
+  if (go_direct == true) {
+    result->r = PARENT_DIRECT;
+  } else {
+    result->r = PARENT_FAIL;
+  }
 }
 
 void
