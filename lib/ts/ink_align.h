44a2a13ffc2dfe47c10f1c66c5ac521ceb844c23
Jacob Ingersol <someone@aol.com>
2017-05-31 11:07:46 -0600

    Revert "only this change directly seems useful for alignment issues"
    This reverts commit 00f3b898984300d8b644359876f22893b05e7a8d.

--- lib/ts/ink_align.h
+++ lib/ts/ink_align.h
@@ -25,27 +25,33 @@
 #ifndef _ink_align_h_
 #define _ink_align_h_
 
-#include <memory>
+#include "ts/ink_time.h"
+
+union Alias32 {
+  uint8_t byte[4];
+  int32_t i32;
+  uint32_t u32;
+};
+
+union Alias64 {
+  uint8_t byte[8];
+  int32_t i32[2];
+  uint32_t u32[2];
+  int64_t i64;
+  uint64_t u64;
+  ink_time_t i_time;
+};
 
 /**
  * Alignment macros
  */
 
 #define INK_MIN_ALIGN 8
-
 /* INK_ALIGN() is only to be used to align on a power of 2 boundary */
-#define INK_ALIGN(size, boundary)  aligned_spacing(size,boundary)
+#define INK_ALIGN(size, boundary) (((size) + ((boundary)-1)) & ~((boundary)-1))
 
 /** Default alignment */
 #define INK_ALIGN_DEFAULT(size) INK_ALIGN(size, INK_MIN_ALIGN)
-
-static inline size_t
-aligned_spacing(size_t len, size_t block=INK_MIN_ALIGN)
-{
-    void *ptr = reinterpret_cast<void*>(len); // pointer from zero
-    // next size >= len to get next a new aligned block
-    return reinterpret_cast<size_t>( std::align(block, 0, ptr, block) );
-}
 
 //
 // Move a pointer forward until it meets the alignment width.
@@ -53,8 +59,13 @@ aligned_spacing(size_t len, size_t block=INK_MIN_ALIGN)
 static inline void *
 align_pointer_forward(const void *pointer_, size_t alignment)
 {
-    // next ptr >= pointer_ to get next a new aligned block
-    return std::align(alignment, 0, const_cast<void*&>(pointer_), alignment); 
+  char *pointer = (char *)pointer_;
+  //
+  // Round up alignment..
+  //
+  pointer = (char *)INK_ALIGN((ptrdiff_t)pointer, alignment);
+
+  return (void *)pointer;
 }
 
 //
@@ -62,12 +73,26 @@ align_pointer_forward(const void *pointer_, size_t alignment)
 // and zero out the contents of the space you're skipping over.
 //
 static inline void *
-align_pointer_forward_and_zero(void *pointer_, size_t alignment)
+align_pointer_forward_and_zero(const void *pointer_, size_t alignment)
 {
-    size_t left = alignment;
-    void *aptr = std::align(alignment, 0, pointer_, left); 
-    memset( pointer_, '\0', alignment - left ); // zero bytes before new block
-    return aptr;
+  char *pointer = (char *)pointer_;
+  char *aligned = (char *)INK_ALIGN((ptrdiff_t)pointer, alignment);
+  //
+  // Fill the skippings..
+  //
+  while (pointer < aligned) {
+    *pointer = 0;
+    pointer++;
+  }
+
+  return (void *)aligned;
 }
+
+//
+// We include two signatures for the same function to avoid error
+// messages concerning coercion between void* and unsigned long.
+// We could handle this using casts, but that's more prone to
+// errors during porting.
+//
 
 #endif
