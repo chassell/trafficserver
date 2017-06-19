e2b99feb6ac0d52e8e32387e4bed072b2716f6f6
Christopher Hassell <christopher_hassell@comcast.com>
2017-06-19 16:02:10 -0600

    changes needed to ifdef-block freelists so that caching is from jemalloc instead

--- lib/ts/Arena.h
+++ lib/ts/Arena.h
@@ -24,6 +24,40 @@
 #ifndef __ARENA_H__
 #define __ARENA_H__
 
+#if HAVE_LIBJEMALLOC
+#include <memory>
+#include <jemalloc/jemalloc.h>
+
+struct Arena : public std::allocator<uint64_t>
+{
+  void *alloc(size_t size) { return allocate((size+1)/sizeof(uint64_t)); }
+  void free(void *mem, size_t size) { deallocate(static_cast<uint64_t*>(mem), size); }
+
+  char *str_alloc(size_t len) { return static_cast<char*>(alloc(len+1)); }
+  char *str_store(const char *str, size_t len)
+  {
+     char *mem = str_alloc(len);
+     memcpy(mem, str, len);
+     mem[len] = '\0';
+     return mem;
+  }
+
+  void str_free(char *str) { free(str,0); }
+
+  // does it the long hard way
+  static size_t str_length(const char *str) 
+  { 
+    size_t l = ::sallocx(str,0);
+    while ( l && ! str[--l] ) 
+       { }
+    return l+1; // assume nul is ending
+  }
+
+  static void reset() { }
+};
+
+#else
+
 #include <sys/types.h>
 #include <memory.h>
 #include "ts/ink_assert.h"
@@ -165,5 +199,7 @@ Arena::str_store(const char *str, size_t len)
 
   return mem;
 }
+
+#endif
 
 #endif /* __ARENA_H__ */
