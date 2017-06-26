1effa2f860f598a3fd4579224627c577a018e4bf
Christopher Hassell <christopher_hassell@comcast.com>
2017-06-26 11:52:02 -0600

    putting useful changes back in again

--- lib/ts/hugepages.cc
+++ lib/ts/hugepages.cc
@@ -177,6 +177,20 @@ chunk_hooks_t const huge_nodump_hooks = {
   &huge_split, &huge_merge
 };
 
+const chunk_hooks_t jemallct_huge_hooks = { 
+  &huge_normal_alloc, &huge_dalloc,
+  &huge_commit, &huge_decommit,
+  &huge_purge;,
+  &huge_split, &huge_merge
+};
+
+const chunk_hooks_t jemallct_huge_nodump_hooks = { 
+  &huge_nodump_alloc, &huge_dalloc,
+  &huge_commit, &huge_decommit,
+  &huge_purge;,
+  &huge_split, &huge_merge
+};
+
 #if defined(linux)
 
 //
