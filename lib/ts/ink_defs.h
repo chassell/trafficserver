9ad1ad72f85dcf5f3b5bcebdc19f2c5f4b24764a
Christopher Hassell <christopher_hassell@comcast.com>
2017-07-25 14:48:44 -0600

    needed for some with hwloc version 7

--- lib/ts/ink_defs.h
+++ lib/ts/ink_defs.h
@@ -118,6 +118,9 @@ template<typename T, typename U> constexpr size_t offset_of(U T::*member)
 
 #if TS_USE_HWLOC
 #include <hwloc.h>
+#if HWLOC_API_VERSION < 0x00010b00
+#define HWLOC_OBJ_NUMANODE  HWLOC_OBJ_NODE
+#endif
 #elif __cplusplus
 // some signatures need this
 using hwloc_obj_type_t = int; 
